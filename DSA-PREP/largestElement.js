1d7effbd40ff2527a95646e50ed195e899f21725
Vasav Prashar <prashar.vasav@tftus.com>
2024-08-08 22:56:29 +0530

    first commit

---
+++ DSA-PREP/largestElement.js
@@ -0,0 +1,24 @@
+// Example 1:
+// Input:
+//  arr[] = {2,5,1,3,0};
+// Output:
+//  5
+// Explanation:
+//  5 is the largest element in the array.
+
+// Example2:
+// Input:
+//  arr[] = {8,10,5,7,9};
+// Output:
+//  10
+// Explanation:
+//  10 is the largest element in the array.
+
+function maxValue(arr) {
+  let max = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
+console.log(maxValue([2, 5, 1, 3, 0]));
